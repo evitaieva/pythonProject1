f6c8b6d36df31df8b3e781ecb1eee7d409e9099c
evitaieva <868717711a@gmail.com>
2024-09-23 19:49:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+print("labas")
+print('labas')
+print(5+5)
+print("ka jus?")
+print(13 / 5)
+print(13 % 5)
+print(13 // 5)
+print(type(5))
+print(type("5"))
+
+
+#duomenu tipai
+skaiciu = 20
+sveikasSkaicius = 20
+skaiciusSuKableliu = 12.5
+tekstas = "as tekstas"
+teiginys = True #boolean False/True
+
+print(15)
+print(12)
+print(89)
+print(5.8)
+print(78*2)
+print(5+3)
+print(7/2)
+print(85-32)
+print(round(5.2 * 3, 1))
+print(9.4 * 0.5)
+print(4.2 / 2)
+print(7+2*3)
+print((7+2)*3)
+print(52+74+32)
+print(87-65+14)
+print(round(79/(5-2), 3))
+print(2**4)
+print(8**3)
+print(14**0.5)
+print(2%2)
+print(3%2)
+print(11%2)
+print(13%2)
+print(10%2)
+print(15%2)
+print(15%3)
+print(15%4)
+print(15%4)
+print(15%5)
+print(15%6)
+print(15%7)
+print(15%8)
+print(15%9)
+print(5//2)
+print(6//3)
+print(6//4)
+print(80//3)
+print(45//4)
+print(45//3)
+print(type(6))
+print(type(2.5))
+print(type(157))
+print(type(-8.2))
+print(type(2/5))
+print(type(8+9))
+print(type(8.5 + 9))
+
+vardas = 'Evita'
+pavarde = 'Ieva'
+amzius = 87
+ugis = 190
+svoris = 190
+aukstojiMokykla = 'Vilniaus Universitetas'
+akademinesGrupesKodas = 2
+kursas = 5
+studijuProgramosPavadinimas = 'princess'
+atsiskaitytuKredituSkaicius = 999
+
+print(vardas)
+print(pavarde)
+print(amzius)
+print(ugis)
+print(svoris)
+print(aukstojiMokykla)
+print(akademinesGrupesKodas)
+print(kursas)
+print(studijuProgramosPavadinimas)
+print(atsiskaitytuKredituSkaicius)
+
+
+pavadinimas = 'miestas'
+valstybe  = 'Lietuva'
+apskritis = 'Alytaus'
+ikurimoData = 1928
+meras = 'pakolkas cesiulis'
+plotasKvM = 45
+gyventojuSkaicius = 50996
+
+print( pavadinimas, type(pavadinimas))
+print( valstybe, type(valstybe))
+print( apskritis, type(apskritis))
+print( ikurimoData, type(ikurimoData))
+print( meras, type(meras))
+print( plotasKvM, type(plotasKvM))
+print( gyventojuSkaicius, type(gyventojuSkaicius))
+
+myName = 'Evita'
+print("mano vardas yra", myName)
+
+akademineGrupe = 'papuolei'
+print('Akademine Grupe:', akademineGrupe)
+vidurkis = 1000000000
+print('Vidurkis:', vidurkis)
+
+stebuklingasZodis = 'as'
+print(stebuklingasZodis, stebuklingasZodis, stebuklingasZodis, stebuklingasZodis, stebuklingasZodis)
+
+vardas = 'Evita'
+amzius = '16'
+bandymas = 'Its now or never'
+
+print(vardas, amzius)
+print(bandymas)
+
+tekstas  = 'Scream for me, screamer\nIm a rock believer\nJust like you\nCome on, scream for me\nIm a rock believer like you\nJust like you'
+print(tekstas)
+
+print('***')
+print('* *')
+print('***')
+
+
+pavadinimas = 'prancuzu buldogas'
+amzius = '(2 m.)'
+kailioSpalva = 'balta'
+svoris  = '18 kg'
+print(pavadinimas, 'yra suo,',amzius, 'turi' , kailioSpalva, 'kailio spalva', 'ir sveria', svoris,'.' )
+
+number = 2020
+print(vardas = 'Evita'
+amzius = '16'
+bandymas = 'Its now or never'
+
+print(vardas, amzius)
+print(bandymas)
+
+tekstas  = 'Scream for me, screamer\nIm a rock believer\nJust like you\nCome on, scream for me\nIm a rock believer like you\nJust like you'
+print(tekstas)
+
+print('***')
+print('* *')
+print('***')
+
+
+pavadinimas = 'prancuzu buldogas'
+amzius = '(2 m.)'
+kailioSpalva = 'balta'
+svoris  = '18 kg'
+print(pavadinimas, 'yra suo,',amzius, 'turi' , kailioSpalva, 'kailio spalva', 'ir sveria', svoris,'.' )
---
+++ main1.py
@@ -0,0 +1,307 @@
+from cgi import print_exception
+
+print("labas")
+print('labas')
+print(5+5)
+print("ka jus?")
+print(13 / 5)
+print(13 % 5)
+print(13 // 5)
+print(type(5))
+print(type("5"))
+
+
+#duomenu tipai
+skaiciu = 20
+sveikasSkaicius = 20
+skaiciusSuKableliu = 12.5
+tektas = "as tekstas"
+teiginys = True #boolean False/True
+
+print(15)
+print(12)
+print(89)
+print(5.8)
+print(78*2)
+print(5+3)
+print(7/2)
+print(85-32)
+print(round(5.2 * 3, 1))
+print(9.4 * 0.5)
+print(4.2 / 2)
+print(7+2*3)
+print((7+2)*3)
+print(52+74+32)
+print(87-65+14)
+print(round(79/(5-2), 3))
+print(2**4)
+print(8**3)
+print(14**0.5)
+print(2%2)
+print(3%2)
+print(11%2)
+print(13%2)
+print(10%2)
+print(15%2)
+print(15%3)
+print(15%4)
+print(15%4)
+print(15%5)
+print(15%6)
+print(15%7)
+print(15%8)
+print(15%9)
+print(5//2)
+print(6//3)
+print(6//4)
+print(80//3)
+print(45//4)
+print(45//3)
+print(type(6))
+print(type(2.5))
+print(type(157))
+print(type(-8.2))
+print(type(2/5))
+print(type(8+9))
+print(type(8.5 + 9))
+
+vardas = 'Evita'
+pavarde = 'Ieva'
+amzius = 87
+ugis = 190
+svoris = 190
+aukstojiMokykla = 'Vilniaus Universitetas'
+akademinesGrupesKodas = 2
+kursas = 5
+studijuProgramosPavadinimas = 'princess'
+atsiskaitytuKredituSkaicius = 999
+
+print(vardas)
+print(pavarde)
+print(amzius)
+print(ugis)
+print(svoris)
+print(aukstojiMokykla)
+print(akademinesGrupesKodas)
+print(kursas)
+print(studijuProgramosPavadinimas)
+print(atsiskaitytuKredituSkaicius)
+
+
+pavadinimas = 'miestas'
+valstybe  = 'Lietuva'
+apskritis = 'Alytaus'
+ikurimoData = 1928
+meras = 'pakolkas cesiulis'
+plotasKvM = 45
+gyventojuSkaicius = 50996
+
+print( pavadinimas, type(pavadinimas))
+print( valstybe, type(valstybe))
+print( apskritis, type(apskritis))
+print( ikurimoData, type(ikurimoData))
+print( meras, type(meras))
+print( plotasKvM, type(plotasKvM))
+print( gyventojuSkaicius, type(gyventojuSkaicius))
+
+myName = 'Evita'
+print("mano vardas yra", myName)
+
+akademineGrupe = 'papuolei'
+print('Akademine Grupe:', akademineGrupe)
+vidurkis = 1000000000
+print('Vidurkis', ':', vidurkis)
+
+stebuklingasZodis = 'as'
+print(stebuklingasZodis, stebuklingasZodis, stebuklingasZodis, stebuklingasZodis, stebuklingasZodis)
+
+vardas = 'Evita'
+amzius = '16'
+bandymas = 'Its now or never'
+
+print(vardas, amzius)
+print(bandymas)
+
+tekstas  = 'Scream for me, screamer\nIm a rock believer\nJust like you\nCome on, scream for me\nIm a rock believer like you\nJust like you'
+print(tekstas)
+
+print('***')
+print('* *')
+print('***')
+
+
+pavadinimas = 'prancuzu buldogas'
+amzius = '(2 m.)'
+kailioSpalva = 'balta'
+svoris  = '18 kg'
+print(pavadinimas, 'yra suo,',amzius, 'turi' , kailioSpalva, 'kailio spalva', 'ir sveria', svoris + '.' )
+print(f'{pavadinimas} yra suo, {amzius} turi {kailioSpalva} kailio spalva, ir sveria {svoris}.' )
+
+skaicius = 2020
+print(skaicius, skaicius, skaicius, skaicius, skaicius, sep="")
+print(skaicius * 5)
+print(f'{skaicius}{skaicius}{skaicius}{skaicius}{skaicius}')
+skaicius = 2020
+print(skaicius, skaicius, skaicius, skaicius, skaicius)
+
+print('*  ')
+print('** ')
+print('***')
+
+print('Hold on tight, slide a little closer\nUp so high stars are on our shoulders\nTime flies by dont close your eyes')
+
+print('+---------+--------+')
+print('| NAME    |  AGE   |')
+print('+---------+--------+')
+print('|  Tomas  |  24    |')
+print('|  Jonas  |  34    |')
+print('|  Ona    |  54    |')
+print('+---------+--------+')
+
+# print("kuo vardu?")
+# vardas = input()
+# print(f"labas {vardas}")
+#
+# vardas = input("kuo tu vardu?")
+# print(f"labas {vardas}")
+
+#print ("dvieju sveiku skaiciu sumavimas skaiciuotuvas")
+#print("pirmas skaicius:")
+#sk1 = int(input())
+#print(type(sk1))
+#print("antras skaicius")
+#sk2 = int(input())
+
+#print(f'{sk1} + {sk2} = {sk1 + sk2}')
+
+sk1 = 1
+sk2 = 2
+sk3 = sk1 + sk2
+print(sk1 + sk2)
+
+
+sk1 = 1
+sk2 = 2
+sk3 = 3
+print(sk1 + sk2 + sk3)
+print(sk1 - sk2 - sk3)
+print(sk1 * sk2 * sk3)
+print(sk1 / sk2 / sk3)
+
+a = 2
+b = 5
+print(a * 5)
+print(b * 2)
+print('a:', a)
+print('b:', b)
+print('multiplication5:', a * 5)
+print('multiplication2:', b * 2)
+
+#varda, amzius, ir kodel pasirinko programavima
+
+#print('koks Jusu vardas?')
+#vardas = input()
+#print('kiek Jums metu?')
+#amzius = input()
+#print('kodel pasirinkote programavima?' )
+#atsakymas = input()
+
+#print('prasau iveskite skaiciu')
+#ivestasSkaicius = int(input())
+
+#skaiciusKvadratu = ivestasSkaicius * ivestasSkaicius
+#print('skaiciusKvadratu:', skaiciusKvadratu)
+
+#print('prasau iveskite simboli')
+#sim = input()
+#print(sim)
+#print('\n', sim, '\n', sim*2, '\n', sim*3)
+
+#varda, amziu, ugi
+#rint('kuo Jus vardu?')
+#vardas = input()
+#print('koks Jusu amzius?')
+#amzius = input()
+#print('koks Jusu ugis?')
+#ugis = input()
+
+#print('prasau parasykite savo 3 pazymius')
+
+#paz1 = int(input())
+#paz2 = int(input())
+#paz3 = int(input())
+#suma = paz1 + paz2 + paz3
+#vid = (paz1 + paz2 + paz3) / 3
+#print(f'{paz1} + {paz2} + {paz3}) = {suma}')
+#print(f'({paz1} + {paz2} + {paz3}) / 3 = {vid}')
+
+#print('prasau parasykite savo 5 pazymius')
+
+#paz1 = int(input())
+#paz2 = int(input())
+#paz3 = int(input())
+#paz4 = int(input())
+#paz5 = int(input())
+#vid = (paz1 + paz2 + paz3 + paz4 + paz5) / 5
+#print(f'({paz1} + {paz2} + {paz3} + {paz4} + {paz5}) / 5 = {vid}')
+
+#print('prasau parasykite metrus')
+#metrai = int(input())
+#centimetrai = metrai / 100
+#print(f'({metrai}  / 100 = {centimetrai}')
+#milimetrai = metrai / 1000
+#print(f'({metrai}  / 1000 = {milimetrai}')
+#kilometrai = metrai * 1000
+#print(f'({metrai}  * 1000 = {kilometrai}')
+
+#print('prasau parasykite 2 skaicius')
+#skai1 = int(input())
+#skai2 = int(input())
+#plus = skai1 + skai2
+#minus = skai1 - skai2
+#multiplication = skai1 * skai2
+#division = skai1 / skai2
+#print(f'{skai1} + {skai2} = {plus}')
+#print(f'{skai1} - {skai2} = {minus}')
+#print(f'{skai1} * {skai2} = {multiplication}')
+#print(f'{skai1} / {skai2} = {division}')
+
+#print('prasau parasykite 2 skaicius')
+#skai1 = int(input())
+#skai2 = int(input())
+#residual =  skai1 / skai2
+#print(f'{residual}')
+
+#print('prasau parasykite 2 skaicius')
+#skai1 = int(input())
+#skai2 = int(input())
+#pirAntroLaipsniu = skai1 * (skai2 * skai2)
+#print(f'{pirAntroLaipsniu}')
+
+#print('prasau parasykite savo amziu')
+#amzius= int(input())
+
+#if amzius < 18:
+    #print('Jusu amzius per mazas')
+#if amzius >= 18:
+   # print('Jusu amzius tinkamas')
+
+#skaicius = 5
+#if skaicius > 0:
+ #   print ('skaicius teigaimas')
+ #   if skaicius <0:
+ #       print ('skaicius neigiamas')
+ #       if skaicius == 0:
+  #          print('skaicius lygus 0')
+
+
+
+    # print('prasau parasykite savo amziu')
+    # amzius = int(input())
+    # if amzius < 18:
+    #     print('jus per jaunas')
+    # elif amzius == 18:
+    #     print('jus ant ribos')
+    # else:
+    #     print('jusu amzius tinkamas')
+
